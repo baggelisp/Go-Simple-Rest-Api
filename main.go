59aeb982811f8792d3c9b1f894bac1b91b3e83d2
Vangelis Spathonis <baggelisp.kef@hotmail.com>
2020-01-07 22:54:13 +0200

    adding users

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -12,37 +14,111 @@ func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome home!")
 }
 
-func get(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "get called"}`))
+type user struct {
+	id       string `json:"id"`
+	name     string `json:"name"`
+	username string `json:"username"`
+	email    string `json:"email"`
 }
 
-func post(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+type allUser []user
+
+var users = allUser{
+	{
+		id:       "1",
+		name:     "Donna Nash",
+		email:    "donnanash@rockyard.com",
+		username: "Carver",
+	},
+	{
+		id:       "2",
+		name:     "Joann Albert",
+		email:    "joannalbert@rockyard.com",
+		username: "Felecia",
+	},
+	{
+		id:       "3",
+		name:     "Hamilton Guerrero",
+		email:    "hamiltonguerrero@rockyard.com",
+		username: "Rosario",
+	},
+	{
+		id:       "4",
+		name:     "Beverley Oliver",
+		email:    "beverleyoliver@rockyard.com",
+		username: "Dianne",
+	},
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var newUser user
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the user title and description only in order to update")
+	}
+
+	json.Unmarshal(reqBody, &newUser)
+	users = append(users, newUser)
 	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(`{"message": "post called"}`))
+
+	json.NewEncoder(w).Encode(newUser)
 }
 
-func put(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write([]byte(`{"message": "put called"}`))
+func getOneUser(w http.ResponseWriter, r *http.Request) {
+	userID := mux.Vars(r)["id"]
+
+	for _, singleUser := range users {
+		if singleUser.id == userID {
+			json.NewEncoder(w).Encode(singleUser)
+		}
+	}
 }
 
-func delete(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "delete called"}`))
+func getAllUsers(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("%+v\n", users)
+	json.NewEncoder(w).Encode(users)
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	userID := mux.Vars(r)["id"]
+	var updatedUser user
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the user title and description only in order to update")
+	}
+	json.Unmarshal(reqBody, &updatedUser)
+
+	for i, singleUser := range users {
+		if singleUser.id == userID {
+			singleUser.name = updatedUser.name
+			singleUser.username = updatedUser.username
+			singleUser.email = updatedUser.email
+			users = append(users[:i], singleUser)
+			json.NewEncoder(w).Encode(singleUser)
+		}
+	}
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	userID := mux.Vars(r)["id"]
+
+	for i, singleUser := range users {
+		if singleUser.id == userID {
+			users = append(users[:i], users[i+1:]...)
+			fmt.Fprintf(w, "The user with ID %v has been deleted successfully", userID)
+		}
+	}
 }
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
-	router.HandleFunc("/get", get).Methods(http.MethodGet)
-	router.HandleFunc("/post", post).Methods(http.MethodPost)
-	router.HandleFunc("/put", put).Methods(http.MethodPut)
-	router.HandleFunc("/delete", delete).Methods(http.MethodDelete)
+	router.HandleFunc("/user", createUser).Methods("POST")
+	router.HandleFunc("/users", getAllUsers).Methods("GET")
+	router.HandleFunc("/users/{id}", getOneUser).Methods("GET")
+	router.HandleFunc("/users/{id}", updateUser).Methods("PATCH")
+	router.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
